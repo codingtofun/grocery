ff65b02d3264630be670dfb10d734c4baaaf0201
codingtofun <codingtofun@gmail.com>
2025-03-12 16:31:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const content = document.getElementById("categories-content");
+
+TEST TEXT POOJA
+
+NAVANSHU
+
+veeraj
+
+daddy
+
+const moveLeft = () => {
+  content.scrollBy({
+    left: 200,
+    behavior: "smooth"
+  });
+};
+
+const moveRight = () => {
+  content.scrollBy({
+    left: -200,
+    behavior: "smooth"
+  });
+};
+
+INDIA
